4ece019864ff8bf80915b07c4595a4326499b729
sandeep <schourey1988@gmail.com>
2015-07-22 20:41:07 +0530

    add

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,17 @@
 class WelcomeController < ApplicationController
+    respond_to :html, :js
   def index
   	@users = User.all
   end
   def new
-    @user = User.create(:email => "bbb@gmail.com", :password=> "12345678", :password_confirmation=> "12345678")
+    @user = User.create(:email => "qqqqqqqqq@gmail.com", :password=> params[:password], :password_confirmation=> params[:password_confirmation])
     redirect_to root_path
   end
+
   def create
   end
 end
-private
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
-    end
+# private
+#     def user_params
+#       params.require(:user).permit(:email, :password, :password_confirmation)
+#     end
