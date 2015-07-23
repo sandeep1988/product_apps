6f3f99acb2ae54c118a9ee3c412a47137c57bfa5
sandeep <schourey1988@gmail.com>
2015-07-23 12:48:08 +0530

    ajax create

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,19 @@
 class WelcomeController < ApplicationController
-    respond_to :html, :js
+     respond_to :html, :js
   def index
   	@users = User.all
   end
+  # def new 
+  #   @user = User.new
+  # end
   def new
-    @user = User.create(:email => "qqqqqqqqq@gmail.com", :password=> params[:password], :password_confirmation=> params[:password_confirmation])
-    redirect_to root_path
+    @user = User.create(:email => params[:email], :password=> params[:password], :password_confirmation=> params[:password_confirmation])
+    respond_to do |format|
+      format.html { render :show, status: :created, location: @user }
+        format.json 
+        format.js
   end
-
+end
   def create
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
+  resources :products
   devise_for :users
-  get 'welcome/index'
+ # get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
-post 'welcome/new'
+  get 'welcome/new'
+  # post 'welcome/create'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
